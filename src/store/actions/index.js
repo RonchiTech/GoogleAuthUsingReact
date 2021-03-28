64d68331a9d3fd46fcc922ac394e551fe0b20db7
ronchitech <ronchibusiness@gmail.com>
2021-03-28 16:56:18 +0800

    added action creators

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -12,6 +12,50 @@ export const signOut = () => {
 
 export const createStream = (formValues) => {
   return async (dispatch) => {
-    streams.post('/streams', formValues);
+    const { data } = await streams.post('/streams', formValues);
+    dispatch({
+      type: actionTypes.CREATE_STREAM,
+      payload: data,
+    });
+  };
+};
+
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const { data } = await streams.get('/streams');
+    dispatch({
+      type: actionTypes.FETCH_STREAMS,
+      payload: data,
+    });
+  };
+};
+
+export const fetchStream = (id) => {
+  return async (dispatch) => {
+    const { data } = await streams.get(`/streams/${id}`);
+    dispatch({
+      type: actionTypes.FETCH_STREAM,
+      payload: data,
+    });
+  };
+};
+
+export const updateStream = (id, updatedValues) => {
+  return async (dispatch) => {
+    const { data } = await streams.patch(`/streams/${id}`, updatedValues);
+    dispatch({
+      type: actionTypes.UPDATE_STREAM,
+      payload: data,
+    });
+  };
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({
+      type: actionTypes.DELETE_STREAM,
+      payload: id,
+    });
   };
 };
